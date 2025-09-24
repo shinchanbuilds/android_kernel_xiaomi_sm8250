19a4053198c3d8c37ed08717cd0c533c7431e18b
CuriousNom <aryanbiswas7585@gmail.com>
2025-09-24 03:30:10 +0000

    drivers/cpufreq: Fix input boost misleading indentation
    Change-Id: I06ff8614c0e241ee906afd51e6f35c27

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -96,12 +96,13 @@ static unsigned int get_input_boost_freq(struct cpufreq_policy *policy)
 {
 	unsigned int freq;
 
-	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
+	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask)) {
 		freq = max(input_boost_freq_little, cpu_freq_min_little);
-	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
+	} else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
 		freq = max(input_boost_freq_big, cpu_freq_min_big);
-	else
+	} else {
                 freq = max(input_boost_freq_prime, cpu_freq_min_prime);
+        }
                 return min(freq, policy->max);
 
 }
@@ -110,12 +111,13 @@ static unsigned int get_max_boost_freq(struct cpufreq_policy *policy)
 {
 	unsigned int freq;
 
-	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
+	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask)) {
 		freq = max(max_boost_freq_little, cpu_freq_min_little);
-	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
+	} else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
 		freq = max(max_boost_freq_big, cpu_freq_min_big);
-        else
+        } else {
                 freq = max(max_boost_freq_prime, cpu_freq_min_prime);
+        }
 		return min(freq, policy->max);
 }
 
